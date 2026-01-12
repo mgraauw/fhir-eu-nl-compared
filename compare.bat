9b08d1b8b0cbc56abdbe36e2798e225d0bd0f294
Marc de Graauw <6449289+mgraauw@users.noreply.github.com>
2026-01-12 15:38:59 +0100

    Main compare

---
+++ compare.bat
@@ -0,0 +1,5 @@
+java -jar ../../validator_cli.jar -compare -dest compared -version 4.0 ^
+-ig hl7.fhir.eu.base#2.0.0-ballot ^
+-ig nictiz.fhir.nl.r4.nl-core#0.12.0-beta.4 ^
+-left http://hl7.eu/fhir/base/StructureDefinition/allergyIntolerance-eu-core ^
+-right http://nictiz.nl/fhir/StructureDefinition/nl-core-AllergyIntolerance
